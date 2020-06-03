04874dad5de8a3c8c8519698ae3e3817acc039d2
Katy-Wicks111 <64050160+Katy-Wicks111@users.noreply.github.com>
2020-06-03 12:18:19 -0500

    Update model.cxx

--- model.cxx
+++ model.cxx
@@ -27,6 +27,7 @@ void place_piece(int pocket_num){
         pieces_[pocket_num].pocket = pocket_num;
 }
 
+/*
 void Model::play_move(Pocket p)
 {
     int pocket_num= p.pieces;
@@ -35,4 +36,58 @@ void Model::play_move(Pocket p)
         pockets_[(p.location+i)%14]+=1;
     }
 }
+*/
+
+//formerly play move
+void Model::distribute_tokens(Pocket p, Player player)
+{
+    int pocket_num= p.pieces;
+    for(int i=0; i<=pocket_num; i++)
+    {
+        int temp= (p.location + i)%14;
+        if(player==Player::first) {
+            if(temp !=0 || (temp==0 && i==pocket_num)) {
+                pockets_[temp].pieces += 1;
+            }
+        } else {
+            if(temp !=7 || (temp==7 && i==pocket_num)) {
+                pockets_[temp].pieces += 1;
+            }
+
+
+        }
+    }
+}
+
+bool player_wins()
+{
+ int total=0;
+ for(int i=1; i<=3; i++)
+ {
+    total = total + pockets_.pieces;
+ }
+ for(int j=11; j<=13; j++)
+ {
+     total = total + pockets_.pieces;
+ }
+ return total == 0;
+ //winner_=Player::computer;
+}
+
+bool computer_wins()
+{
+    int total=0;
+    for(int i=4; i<=6; i++)
+    {
+        total = total + pockets_.pieces;
+    }
+    for(int j=8; j<=10; j++)
+    {
+        total = total + pockets_.pieces;
+    }
+    return total == 0;
+    //winner_=Player::computer;
+}
+
+
 
